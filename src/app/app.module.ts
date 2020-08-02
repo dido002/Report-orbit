fd7772e66b36512a2f0ff5804ae71abab279ab7a
Umer <an01985i@gmail.com>
2020-08-02 01:25:53 -0500

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrbitCountComponent } from './orbit-count/orbit-count.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
+import { OrbitReportComponent } from './orbit-report/orbit-report.component';
+import { Satellite } from './satellite.ts/satellite.ts.component';
+import { SatelliteComponent } from './satellite/satellite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrbitCountComponent,
+    OrbitListComponent,
+    OrbitReportComponent,
+    Satellite.TsComponent,
+    SatelliteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/orbit-count/orbit-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbit-count',
+  templateUrl: './orbit-count.component.html',
+  styleUrls: ['./orbit-count.component.css']
+})
+export class OrbitCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
